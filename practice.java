bb6a8505ff0e1b7922fabbb02b1bb38e09c30088
ABHISHEK (Passionate Android developer) <abhishektiwari1706@gmail.com>
2024-08-15 00:36:56 -0700

    first commit

---
+++ practice.java
@@ -0,0 +1,19 @@
+
+public class practice {
+    public static void main(String[] args){
+        int n = 91;
+        int count = 0 ;
+        for (int i=n-1; i>2; i--){
+          if(n%i==0){
+            count = count + 1;
+          }
+        }
+        if(count==0){
+            System.out.println("numer is prime");
+        }else{
+            System.out.println("number is not prime");
+        }
+        
+    }
+}
+
